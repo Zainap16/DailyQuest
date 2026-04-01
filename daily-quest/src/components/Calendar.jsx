4fa021ae8137c4c8619a76b7fd82ee67a2f3e644
Ziah <zvanblerck1@gmail.com>
2026-04-01 23:09:31 +0200

    added some notes

--- daily-quest/src/components/Calendar.jsx
+++ daily-quest/src/components/Calendar.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import timeGridPlugin from "@fullcalendar/timegrid";
-// import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 function Calendar() {
 return(
        <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView='dayGridMonth'
         weekends={false}
+        // dateClick={handleDateClick}
+        selectable={true}
+        
       />
 
 )
 }
 
 export default Calendar;
+// To actually use it like a task system, you enable:
+
+// Date clicking
+// → user clicks a day to add a task
+// Event clicking
+// → user clicks a task to edit/delete it
+// Dragging
+// → move tasks to another day
+// Selecting
+// → highlight multiple days for longer tasks
+
+// → you capture that click
+// → ask for task info (like a popup)
+// → add it to your event list
